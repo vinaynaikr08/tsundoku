d34491bfba8f211befd8b0538b0a19bec180061c
Eric Park <me@ericswpark.com>
2024-01-26 18:58:45 -0500

    mobile: refactor out initial launch screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,31 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
-import Colors from './Constants/Colors';
+import { InitialLaunchScreen } from './Screens/InitialLaunchScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>tsundoku</Text>
-      <Text>1. The practice of buying books and not reading them, letting them pile up with other unread books</Text>
-
-      <Pressable onPress={launchStart}>
-        <Text>Replace me with right arrow</Text>
-      </Pressable>
-      <StatusBar style="auto" />
-    </View>
+    <InitialLaunchScreen />
   );
 }
 
-function launchStart() {
-  // TODO: Implement
-  console.log("Open setup");
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.ONBOARDING_BG_PINK,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/Screens/InitialLaunchScreen.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import Colors from '../Constants/Colors';
+
+
+export const InitialLaunchScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>tsundoku</Text>
+            <Text>1. The practice of buying books and not reading them, letting them pile up with other unread books</Text>
+
+            <Pressable onPress={launchStart}>
+                <Text>Replace me with right arrow</Text>
+            </Pressable>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+function launchStart() {
+    // TODO: Implement
+    console.log("Open setup");
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.ONBOARDING_BG_PINK,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
