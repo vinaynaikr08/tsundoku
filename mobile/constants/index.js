2ec32c9ebe08057e3b3cb290ec12f442b1344739
Eric Park <me@ericswpark.com>
2024-01-26 18:01:13 -0500

    mobile: add color constants

---
+++ mobile/constants/index.js
@@ -0,0 +1,5 @@
+// Onboarding colors
+export const ONBOARDING_BG_PINK = "#DF57C1";
+export const ONBOARDING_BG_BRIGHT_GREEN = "#87FFCD";
+export const ONBOARDING_BG_BRIGHT_BLUE = "#22CFE7";
+export const ONBOARDING_BG_PURPLE = "#8C0FEE";
