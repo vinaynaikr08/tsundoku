11be7fa4dac8eabed207a89c086bb13ff78b2894
Eric Park <me@ericswpark.com>
2024-01-26 18:09:19 -0500

    mobile: initial launch screen early draft
    VERY early draft

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import * as Constants from './constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>tsundoku</Text>
+      <Text>1. The practice of buying books and not reading them, letting them pile up with other unread books</Text>
+
+      <Pressable onPress={launchStart}>
+        <Text>Replace me with right arrow</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function launchStart() {
+  // TODO: Implement
+  console.log("Open setup");
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Constants.ONBOARDING_BG_PINK,
     alignItems: 'center',
     justifyContent: 'center',
   },
