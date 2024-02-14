661a3a9de2c100afc2d84eb0bada7b4a20dbabfb
Sarah Luo <sarrstar888@gmail.com>
2024-02-14 11:52:06 -0500

    updated nav bar with react nav library

--- mobile/Components/NavBar/NavBar.tsx
+++ mobile/Components/NavBar/NavBar.tsx
@@ -7,8 +7,11 @@ import React from "react";
 import { useState, useEffect, useRef, useCallback } from "react";
 import Colors from "../../Constants/Colors";
 import { Library } from "../../Screens/Library";
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
   
+const Stack = createNativeStackNavigator();
+
 export const NavBar = () => {
   // home
   
@@ -19,6 +22,17 @@ export const NavBar = () => {
   // social
 
   // profile
+  
+  <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Library"
+          component={Library}
+        //   options={{title: 'Welcome'}}
+        />
+        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
 
 };
   
