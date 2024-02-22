ede4916f977af269e8ea76ff1fbafe94c2eff727
Eric Park <me@ericswpark.com>
2024-02-22 11:02:42 -0500

    backend: disallow self-signed SSL certificates
    We already have certs issued by LE

--- backend/src/app/appwrite.js
+++ backend/src/app/appwrite.js
@@ -5,6 +5,5 @@ export const client = new sdk.Client();
 client
     .setEndpoint(process.env.appwriteEndpoint)
     .setProject(process.env.appwriteProjectID)
-    .setKey(process.env.appwriteAPIKey)
-    .setSelfSigned();
+    .setKey(process.env.appwriteAPIKey);
 
