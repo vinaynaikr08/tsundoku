04902395c3e7fcf291f7846c49955559aa6f255c
Eric Park <me@ericswpark.com>
2024-02-24 17:07:25 -0500

    backend: tests: fix tests and rename spec file

--- backend/tests/api.spec.ts
+++ backend/tests/api.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("api v0 book search", async ({ page }) => {
-  const response = await page.request.get("http://localhost:3000/api/v0/books/search?Harry+Potter");
+  const response = await page.request.get("http://localhost:3000/api/v0/books/search?title=Harry+Potter");
   await expect(response).toBeOK();
 });
