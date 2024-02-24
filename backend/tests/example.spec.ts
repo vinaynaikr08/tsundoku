c5aaeef26c04081c0411b477d1c8596220a2477e
Eric Park <me@ericswpark.com>
2024-02-24 17:01:12 -0500

    backend: add Playwright for testing

---
+++ backend/tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+
+test("api v0 book search", async ({ page }) => {
+  await page.goto("http://localhost:3000/api/v0/books/search");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" }),
+  ).toBeVisible();
+});
