1bbe30d6a4017d40567f408ae82d405ecd04dc27
Eric Park <me@ericswpark.com>
2024-02-25 10:46:20 -0500

    backend: remove unnecessary file

--- backend/src/app/api/v0/bookstatus/bookStatusPermissions.ts
+++
@@ -1,9 +0,0 @@
-import { Permission } from "appwrite";
-
-export function bookStatusPermissions(user_id: string) {
-  return [
-    Permission.read(Role.user(user_id)),
-    Permission.update(Role.user(user_id)),
-    Permission.delete(Role.user(user_id)),
-  ];
-}
