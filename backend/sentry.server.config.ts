33ce3bf5a9cdc4df3cb22c4ff89eb32160972304
Eric Park <me@ericswpark.com>
2024-02-27 23:33:04 -0500

    backend: configure Sentry for error monitoring

---
+++ backend/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://46ca56d013274aa741b8e01770b795f4@o4506822527483904.ingest.sentry.io/4506822529581056",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
