0118f94ebd7d1f3b95c4b0d39f7a6001fb402c03
Eric Park <me@ericswpark.com>
2024-02-28 10:29:28 -0500

    backend: tests: check successful response contents

--- backend/tests/api.spec.ts
+++ backend/tests/api.spec.ts
@@ -4,14 +4,18 @@ test("api v0 book search", async ({ page }) => {
   const response = await page.request.get(
     "http://localhost:3000/api/v0/books/search?title=Harry+Potter",
   );
+  const responseBody = JSON.parse(await response.text());
   await expect(response).toBeOK();
+  await expect(responseBody.message).toContain("Harry Potter");
 });
 
 test("api v0 author search", async ({ page }) => {
   const response = await page.request.get(
     "http://localhost:3000/api/v0/authors/search?name=Robert+Muchamore",
   );
+  const responseBody = JSON.parse(await response.text());
   await expect(response).toBeOK();
+  await expect(responseBody.message).toContain("Robert Muchamore");
 });
 
 test("api v0 book search missing param", async ({ page }) => {
