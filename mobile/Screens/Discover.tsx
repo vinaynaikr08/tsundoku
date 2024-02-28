ee2ef4c105221989e81fce7645e7cbeb241d5223
Eric Park <me@ericswpark.com>
2024-02-28 15:57:21 -0500

    mobile: Discover: style with prettier

--- mobile/Screens/Discover.tsx
+++ mobile/Screens/Discover.tsx
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
-import { View, Text, Platform, Image, FlatList, TouchableWithoutFeedback, Keyboard, TouchableOpacity, Dimensions, KeyboardAvoidingView, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Platform,
+  Image,
+  FlatList,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+  Dimensions,
+  KeyboardAvoidingView,
+  Pressable,
+} from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-
-import { BACKEND_API_BOOK_SEARCH_URL, BACKEND_API_AUTHOR_SEARCH_URL } from "../Constants/URLs";
+import {
+  BACKEND_API_BOOK_SEARCH_URL,
+  BACKEND_API_AUTHOR_SEARCH_URL,
+} from "../Constants/URLs";
 import CarouselTabs from "../Components/DiscoverCarouselTabs/DiscoverCarouselTabs";
 import { NavigationContext } from "../Contexts";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -14,7 +28,7 @@ import { DATA } from "@/Components/BookSearchBar/Genres";
 const Stack = createStackNavigator();
 
 export const Discover = (props) => {
-  const windowHeight = Dimensions.get('window').height; 
+  const windowHeight = Dimensions.get("window").height;
   const [loading, setLoading] = useState(false);
   const [books, setBooks] = useState([]);
   const [authors, setAuthors] = useState([]);
@@ -41,7 +55,7 @@ export const Discover = (props) => {
           image_url: book.editions[0].thumbnail_url,
           isbn_10: book.editions[0].isbn_10,
           isbn_13: book.editions[0].isbn_13,
-          genre: book.genre
+          genre: book.genre,
         };
       });
     }
@@ -62,29 +76,33 @@ export const Discover = (props) => {
       });
     }
 
-    getBooks(search).then((data) => {
-      setBooks(data);
-    }).catch((error: TypeError) => {
-    }).catch((error: any) => {
-      if (error instanceof Error) {
-        setErrorMessage(error.message);
-        setErrorModalVisible(true);
-      } else {
-        throw error;
-      }
-    });
+    getBooks(search)
+      .then((data) => {
+        setBooks(data);
+      })
+      .catch((error: TypeError) => {})
+      .catch((error: any) => {
+        if (error instanceof Error) {
+          setErrorMessage(error.message);
+          setErrorModalVisible(true);
+        } else {
+          throw error;
+        }
+      });
 
-    getAuthors(search).then(data => {
-      setAuthors(data);
-    }).catch((error: TypeError) => {
-    }).catch((error: any) => {
-      if (error instanceof Error) {
-        setErrorMessage(error.message);
-        setErrorModalVisible(true);
-      } else {
-        throw error;
-      }
-    });
+    getAuthors(search)
+      .then((data) => {
+        setAuthors(data);
+      })
+      .catch((error: TypeError) => {})
+      .catch((error: any) => {
+        if (error instanceof Error) {
+          setErrorMessage(error.message);
+          setErrorModalVisible(true);
+        } else {
+          throw error;
+        }
+      });
 
     setLoading(false);
   }, [search]);
@@ -93,7 +111,6 @@ export const Discover = (props) => {
   const updateSearch = (search) => {
     setSearch(search);
   };
-
 
   function checkGenres(value) {
     let noFilter: boolean = true;
@@ -111,20 +128,18 @@ export const Discover = (props) => {
     } else {
       for (let genre of GENRES) {
         if (value == genre.title[0]) {
-          return (genre.state[0]);
-        }
-        else if (value == genre.title[1]) {
-          return (genre.state[1]);
+          return genre.state[0];
+        } else if (value == genre.title[1]) {
+          return genre.state[1];
         }
       }
     }
   }
 
-
   return (
     <NavigationContext.Provider value={navigation}>
       <SafeAreaView style={{ flexGrow: 1, backgroundColor: "white" }}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
           <View>
             <Text
               style={{
@@ -139,51 +154,78 @@ export const Discover = (props) => {
             </Text>
           </View>
         </TouchableWithoutFeedback>
-        <View style={{paddingLeft: 10, paddingBottom: 10, paddingRight: 10}}>
-          <BookSearchBar search={search} updateSearch={updateSearch} newPlaceholder={"Search all books"} loading={loading} showFilter={true} GENRES={GENRES} />
+        <View style={{ paddingLeft: 10, paddingBottom: 10, paddingRight: 10 }}>
+          <BookSearchBar
+            search={search}
+            updateSearch={updateSearch}
+            newPlaceholder={"Search all books"}
+            loading={loading}
+            showFilter={true}
+            GENRES={GENRES}
+          />
         </View>
         <CarouselTabs navigation={navigation} />
-        
-        {search.length > 0 && 
-        <KeyboardAvoidingView 
-          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-          style={{ backgroundColor: '#F7F7F7', width: '100%', position: "absolute", top: '21.5%', zIndex: 100, borderColor: 'black', borderWidth: 0, maxHeight: windowHeight - 230}}>
-          <FlatList data={books.filter(book => checkGenres(book.genre))}
-            style={{flexGrow: 0}} 
-            renderItem={({item}) => {
-              return (
-                <TouchableOpacity onPress={() =>
-                  navigation.navigate("bookInfoModal", { bookInfo: item })
-                }>
-                  <View style={{flexDirection: 'row', paddingTop: 10, paddingBottom: 10}}>
-                    <Image
-                      style={{
-                      width: 80,
-                      height: 100,
-                      justifyContent: "center",
-                      alignItems: "center",
-                      backgroundColor: "white",
-                      shadowColor: "black",
-                      }}
-                      resizeMode="contain"
-                      source={{ uri: item.image_url }}
-                    />
-                    <View style={{ paddingLeft: 10, width: "80%" }}>
-                      <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 20, flexShrink: 1}}>{item.title}</Text>
-                      </View>
-                      <Text>{item.author}</Text>
-                      <Text >ISBN: {item.isbn_13}</Text>
-                    </View>
-                  </View>
-                  <Divider style={{backgroundColor: "black"}}/>
-                </TouchableOpacity>
-              )
-              
-            } 
-          }/>
 
-        </KeyboardAvoidingView>}
+        {search.length > 0 && (
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{
+              backgroundColor: "#F7F7F7",
+              width: "100%",
+              position: "absolute",
+              top: "21.5%",
+              zIndex: 100,
+              borderColor: "black",
+              borderWidth: 0,
+              maxHeight: windowHeight - 230,
+            }}
+          >
+            <FlatList
+              data={books.filter((book) => checkGenres(book.genre))}
+              style={{ flexGrow: 0 }}
+              renderItem={({ item }) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() =>
+                      navigation.navigate("bookInfoModal", { bookInfo: item })
+                    }
+                  >
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        paddingTop: 10,
+                        paddingBottom: 10,
+                      }}
+                    >
+                      <Image
+                        style={{
+                          width: 80,
+                          height: 100,
+                          justifyContent: "center",
+                          alignItems: "center",
+                          backgroundColor: "white",
+                          shadowColor: "black",
+                        }}
+                        resizeMode="contain"
+                        source={{ uri: item.image_url }}
+                      />
+                      <View style={{ paddingLeft: 10, width: "80%" }}>
+                        <View style={{ flexDirection: "row" }}>
+                          <Text style={{ fontSize: 20, flexShrink: 1 }}>
+                            {item.title}
+                          </Text>
+                        </View>
+                        <Text>{item.author}</Text>
+                        <Text>ISBN: {item.isbn_13}</Text>
+                      </View>
+                    </View>
+                    <Divider style={{ backgroundColor: "black" }} />
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </KeyboardAvoidingView>
+        )}
       </SafeAreaView>
     </NavigationContext.Provider>
   );
