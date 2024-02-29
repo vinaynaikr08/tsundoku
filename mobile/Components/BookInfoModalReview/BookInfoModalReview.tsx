387329a12ab1476065a54f0aab139075dc95b1c8
Sarah Luo <sarrstar888@gmail.com>
2024-02-29 13:29:25 -0500

    review tab ui

--- mobile/Components/BookInfoModalReview/BookInfoModalReview.tsx
+++ mobile/Components/BookInfoModalReview/BookInfoModalReview.tsx
@@ -83,7 +83,7 @@ export const BookInfoModalReview = ({ bookInfo }) => {
   };
 
   return (
-    <ScrollView style={{ flex: 1 }}>
+    <View style={{ flex: 1 }}>
       {reviews.length === 0 ? (
         <Text style={{ fontSize: 20, fontWeight: "bold", margin: 30 }}>
           No Reviews
@@ -191,7 +191,7 @@ export const BookInfoModalReview = ({ bookInfo }) => {
           />
         </View>
       )}
-    </ScrollView>
+    </View>
   );
 };
 
