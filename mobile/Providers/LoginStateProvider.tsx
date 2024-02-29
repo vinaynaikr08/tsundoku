5fff1b2224344752f10096eb97fc165236d37a9c
Eric Park <me@ericswpark.com>
2024-02-29 13:14:45 -0500

    mobile: LoginStateProvider: persist login state across app launches
    We check if the previous session is still valid, before letting the
    users in to the app

--- mobile/Providers/LoginStateProvider.tsx
+++ mobile/Providers/LoginStateProvider.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { client } from "@/appwrite";
+import { Account } from "appwrite";
+
+const account = new Account(client);
+
 export const LoginStateContext = React.createContext(null);
 
 function LoginStateProvider({ children }) {
-  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [loggedIn, setLoggedIn] = React.useState(() => {
+    try {
+      (async () => {await account.get()})();
+    } catch (error: any) {
+      return false;
+    }
+    return true;
+  });
 
   return (
     <LoginStateContext.Provider value={{loggedIn, setLoggedIn}}>
