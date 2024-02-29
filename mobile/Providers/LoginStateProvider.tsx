75bebe0c8cc32d9089167f3afa0c1c9c761e6aae
Eric Park <me@ericswpark.com>
2024-02-29 13:16:51 -0500

    mobile: LoginStateProvider: allow for refreshing login state from views

--- mobile/Providers/LoginStateProvider.tsx
+++ mobile/Providers/LoginStateProvider.tsx
@@ -7,21 +7,31 @@ const account = new Account(client);
 
 export const LoginStateContext = React.createContext(null);
 
+function checkLoggedIn() {
+  try {
+    (async () => {
+      await account.get();
+    })();
+  } catch (error: any) {
+    return false;
+  }
+  return true;
+}
+
 function LoginStateProvider({ children }) {
-  const [loggedIn, setLoggedIn] = React.useState(() => {
-    try {
-      (async () => {await account.get()})();
-    } catch (error: any) {
-      return false;
-    }
-    return true;
-  });
+  const [loggedIn, setLoggedIn] = React.useState(checkLoggedIn);
+
+  function refreshLoginState() {
+    setLoggedIn(checkLoggedIn());
+  }
 
   return (
-    <LoginStateContext.Provider value={{loggedIn, setLoggedIn}}>
+    <LoginStateContext.Provider
+      value={{ loggedIn, setLoggedIn, refreshLoginState }}
+    >
       {children}
     </LoginStateContext.Provider>
-  )
+  );
 }
 
 export default LoginStateProvider;
