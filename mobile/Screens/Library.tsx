130a1b673b73a4a25d9d043f3539234ee61f09c8
Eric Park <me@ericswpark.com>
2024-02-29 21:14:27 -0500

    mobile: Library: remove unused imports

--- mobile/Screens/Library.tsx
+++ mobile/Screens/Library.tsx
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
-import { View, Text, Pressable, Button } from "react-native";
-import Carousel from "../Components/Carousel/Carousel";
+import React from "react";
+import { Text } from "react-native";
 import CarouselTabs from "../Components/LibraryCarousel/LibraryCarouselTabs";
-import StarRating from "../Components/StarRating/StarRating";
-import { createStackNavigator } from "@react-navigation/stack";
-import TextReview from "../Components/TextReview/TextReview";
-import { useNavigation } from "@react-navigation/native";
-import { createContext, useContext } from "react";
 
 import { NavigationContext } from "../Contexts";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const Stack = createStackNavigator();
-
 export const Library = (props) => {
   const { navigation } = props;
-  const [isReviewModalVisible, setReviewModalVisible] = useState(false);
+
   return (
     <NavigationContext.Provider value={navigation}>
       <SafeAreaView style={{ flexGrow: 1, backgroundColor: "white" }}>
@@ -35,15 +27,3 @@ export const Library = (props) => {
     </NavigationContext.Provider>
   );
 };
-
-// export const LibraryPage = () => {
-//   return (
-//     <Stack.Navigator screenOptions={{ headerShown: false }}>
-//       <Stack.Screen name="Library" component={Library} />
-//       <Stack.Group screenOptions={{ presentation: "modal" }}>
-//         <Stack.Screen name="StarRating" component={StarRating} />
-//         <Stack.Screen name="TextReview" component={TextReview} />
-//       </Stack.Group>
-//     </Stack.Navigator>
-//   );
-// };
