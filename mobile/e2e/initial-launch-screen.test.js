80d1345ca56d1209a6a5c3436e943a3284e58b30
Eric Park <me@ericswpark.com>
2024-03-15 10:59:22 -0400

    mobile: e2e: match by sign in view ID

--- mobile/e2e/initial-launch-screen.test.js
+++ mobile/e2e/initial-launch-screen.test.js
@@ -13,6 +13,6 @@ describe('Initial launch screen', () => {
 
   it('should show sign in screen after arrow tap', async () => {
     await element(by.id('initial-launch-screen-launch-start-arrow')).tap();
-    await expect(element(by.text('Sign in'))).toBeVisible();
+    await expect(element(by.id('sign-in-view'))).toBeVisible();
   });
 });
