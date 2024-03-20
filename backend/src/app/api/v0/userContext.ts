e6267c3939d604314984ccd3229af1876d3a51e0
Eric Park <me@ericswpark.com>
2024-03-20 18:37:59 -0400

    backend: fix bug with user context fetching wrong env var

--- backend/src/app/api/v0/userContext.ts
+++ backend/src/app/api/v0/userContext.ts
@@ -1,9 +1,11 @@
 const sdk = require("node-appwrite");
 
+import Constants from "@/app/Constants";
+
 export function getUserContextDBAccount(authToken: string) {
   const userClient = new sdk.Client()
-    .setEndpoint(process.env.appwriteEndpoint)
-    .setProject(process.env.appwriteProjectID)
+    .setEndpoint(Constants.APPWRITE_ENDPOINT)
+    .setProject(Constants.APPWRITE_PROJECT_ID)
     .setJWT(authToken);
   const userDB = new sdk.Databases(userClient);
   const userAccount = new sdk.Account(userClient);
