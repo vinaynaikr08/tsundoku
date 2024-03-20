71cabe87fb42d6838b1a8ec33efc87325248a225
Kaley Kwan <kaleymkwan@gmail.com>
2024-03-20 16:27:40 -0400

    complete create custom property form

--- mobile/Components/CreateCustomProperty/CreateCustomProperty.tsx
+++ mobile/Components/CreateCustomProperty/CreateCustomProperty.tsx
@@ -16,10 +16,43 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import SelectDropdown from "react-native-select-dropdown";
+import Slider from "@react-native-community/slider";
+
+function CategoryInputs({ numCategories }) {
+  const [categories, setCategories] = useState(Array(numCategories).fill(""));
+  let category;
+  const handleCategoryChange = (index: number, value: string) => {
+    const newCategories = [...categories];
+    newCategories[index] = value;
+    setCategories(newCategories);
+  };
+
+  const inputs = [];
+  for (let i = 0; i < numCategories; i++) {
+    inputs.push(
+      <TextInput
+        key={i}
+        style={styles.categoryInput}
+        onChangeText={(value) => handleCategoryChange(i, value)}
+        value={category}
+        placeholder="Category Name"
+        placeholderTextColor={Colors.BUTTON_TEXT_GRAY}
+      />,
+    );
+  }
+
+  return (
+    <>
+      {inputs}
+    </>
+  );
+}
 
 function CreateCustomProperty({ navigation }) {
   const [name, setName] = useState("");
   const [type, setType] = useState("");
+  const [numCategories, setNumCategories] = useState(1);
+  const propertyTypes = ["numerical", "categorical", "boolean"];
 
   function dismiss() {
     Alert.alert("Discard property?", "You have an unsaved custom property.", [
@@ -32,8 +65,6 @@ function CreateCustomProperty({ navigation }) {
     ]);
   }
 
-  const propertyTypes = ["numerical", "categorical", "boolean"];
-
   function saveCustomProperty() {}
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -42,7 +73,6 @@ function CreateCustomProperty({ navigation }) {
           display: "flex",
           flex: 1,
           flexDirection: "column",
-          //justifyContent: "center",
           alignItems: "center",
           backgroundColor: "white",
           width: "100%",
@@ -94,6 +124,33 @@ function CreateCustomProperty({ navigation }) {
           }}
           dropdownIconPosition={"left"}
         />
+
+        <View>
+          {type == "categorical" && (
+            <View>
+              <Text style={styles.title}>
+                Number of Categories: {numCategories}{" "}
+              </Text>
+              <Slider
+                style={{
+                  display: "flex",
+                  width: 315,
+                  height: 40,
+                  alignSelf: "center",
+                }}
+                onValueChange={setNumCategories}
+                minimumValue={1}
+                maximumValue={5}
+                step={1}
+                thumbTintColor={Colors.BUTTON_PURPLE}
+                minimumTrackTintColor="black"
+                maximumTrackTintColor={Colors.BOOK_INFO_MODAL_GREY_LINE_COLOR}
+              />
+              <CategoryInputs numCategories={numCategories} />
+            </View>
+          )}
+        </View>
+
         <Pressable onPress={saveCustomProperty} style={styles.saveButton}>
           <Text style={styles.saveButtonText}>Save</Text>
         </Pressable>
@@ -120,6 +177,19 @@ const styles = StyleSheet.create({
     width: "80%",
     height: 50,
   },
+  categoryInput: {
+    display: "flex",
+    margin: 12,
+    marginTop: 5,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: Colors.BOOK_INFO_MODAL_GREY_LINE_COLOR,
+    borderRadius: 15,
+    width: 310,
+    height: 50,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
   saveButton: {
     backgroundColor: Colors.BUTTON_PURPLE,
     padding: 10,
@@ -139,7 +209,7 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     borderWidth: 1,
     borderColor: Colors.BOOK_INFO_MODAL_GREY_LINE_COLOR,
-    marginBottom: 20
+    marginBottom: 20,
   },
 });
 
