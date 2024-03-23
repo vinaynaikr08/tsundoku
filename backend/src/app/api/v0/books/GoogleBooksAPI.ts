596e3845bcba2fd06ddf8202be2b945f5e933911
Eric Park <me@ericswpark.com>
2024-03-23 17:12:05 -0400

    backend: api: GoogleBooksAPI: fix async bug
    You can't return from a promise context any data...

--- backend/src/app/api/v0/books/GoogleBooksAPI.ts
+++ backend/src/app/api/v0/books/GoogleBooksAPI.ts
@@ -1,46 +1,43 @@
+function googleBooksAPIUnavailableError(res_json: any) {
+  console.error(
+    `The Google Books API returned the following response: ${res_json}`,
+  );
+  throw new Error("google-books-api-unavailable");
+}
+
 export namespace GoogleBooksAPI {
-  export function searchBooks(title: string): any {
-    return fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(title)}`,
-    )
-      .then((res) => {
-        res
-          .json()
-          .then((res_json) => {
-            if (typeof res_json === "undefined" || !("items" in res_json)) {
-              console.error(
-                `The Google Books API returned the following response: ${res_json}`,
-              );
-              throw new Error();
-            }
-            return res_json.items;
-          })
-          .catch((error) => {
-            throw error;
-          });
-      })
-      .catch((error) => {
-        throw error;
-      });
+  export async function searchBooks(title: string) {
+    try {
+      const res = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(title)}`,
+      );
+
+      const res_json = await res.json();
+
+      if (typeof res_json === "undefined" || !("items" in res_json)) {
+        googleBooksAPIUnavailableError(res_json);
+      }
+
+      return res_json.items;
+    } catch (error) {
+      throw error;
+    }
   }
 
-  export function getBook(id: string): any {
-    return fetch(`https://www.googleapis.com/books/v1/volumes/${id}`)
-      .then((res) => {
-        res
-          .json()
-          .then((res_json) => {
-            if (typeof res_json === "undefined") {
-              throw new Error();
-            }
-            return res_json;
-          })
-          .catch((error) => {
-            throw error;
-          });
-      })
-      .catch((error) => {
-        throw error;
-      });
+  export async function getBook(id: string) {
+    try {
+      const res = await fetch(
+        `https://www.googleapis.com/books/v1/volumes/${id}`,
+      );
+
+      const res_json = await res.json();
+
+      if (typeof res_json === "undefined") {
+        googleBooksAPIUnavailableError(res_json);
+      }
+      return res_json;
+    } catch (error) {
+      throw error;
+    }
   }
 }
