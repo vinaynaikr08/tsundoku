571345c33096b408cd6859ab28a55299c1289288
Eric Park <me@ericswpark.com>
2024-03-23 15:29:09 -0400

    backend: api: simulator: fix missing parameter

--- backend/src/app/api/v0/simulator/[error_code]/route.ts
+++ backend/src/app/api/v0/simulator/[error_code]/route.ts
@@ -1,6 +1,8 @@
+import { NextRequest } from "next/server";
 import { construct_development_api_response } from "../../dev_api_response";
 
 export async function GET(
+  _request: NextRequest,
   { params }: { params: { error_code: string } },
 ) {
   return construct_development_api_response({
