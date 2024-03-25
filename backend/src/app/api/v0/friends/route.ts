61dcab31233b0cd2553c6f88d41ed9a2be87fc48
Eric Park <me@ericswpark.com>
2024-03-25 13:22:44 -0400

    backend: api: friends: fix query statement

--- backend/src/app/api/v0/friends/route.ts
+++ backend/src/app/api/v0/friends/route.ts
@@ -32,10 +32,10 @@ export async function GET() {
         // You might get an error about `or` not existing in `Query`, this is normal
         // This is because the Appwrite Node SDK doesn't have the type declaration for it *yet*
         // @ts-ignore: upstream issue
-        Query.or(
+        Query.or([
           Query.equal("requester", user_id),
           Query.equal("requestee", user_id),
-        ),
+        ]),
       ],
     );
   } catch (error: any) {
