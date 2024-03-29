bca3947cf056a4ddd6afdc39e45de1f9795b8092
Eric Park <me@ericswpark.com>
2024-03-29 10:54:31 -0400

    mobile: ignore more files and folders with eslint

--- mobile/eslint.config.mjs
+++ mobile/eslint.config.mjs
@@ -7,6 +7,6 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["e2e/", "gradle.js", "babel.config.js"],
+    ignores: ["e2e/", "gradle.js", "babel.config.js", ".detoxrc.js", ".expo/"],
   },
 );
