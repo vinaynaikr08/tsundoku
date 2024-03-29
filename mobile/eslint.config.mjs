a2422efbf7dc49191562fa5b34893466bd412cf0
Eric Park <me@ericswpark.com>
2024-03-29 11:55:15 -0400

    mobile: eslint: expand checked rules

--- mobile/eslint.config.mjs
+++ mobile/eslint.config.mjs
@@ -5,7 +5,16 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigDirName: ".",
+      },
+    },
+  },
   {
     ignores: ["e2e/", "gradle.js", "babel.config.js", ".detoxrc.js", ".expo/"],
   },
