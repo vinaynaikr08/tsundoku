6a2f14808fce71b98b6ee58200cd8cb7f195a384
samliu <liu3420@purdue.edu>
2024-03-28 22:50:49 -0400

    Implemented notifications overlay in community

--- mobile/Screens/CommunityScreen.tsx
+++ mobile/Screens/CommunityScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
   View,
   Text,
@@ -6,25 +6,58 @@ import {
   Keyboard,
   Dimensions,
   StyleSheet,
+  TouchableOpacity,
+  FlatList,
+
 } from "react-native";
 import { DATA } from "@/Components/BookSearchBar/Genres";
-import { Databases, Query } from "appwrite";
+import { Databases, Query, Account } from "appwrite";
 import { client } from "@/appwrite";
 import CommunityTabs from "../Components/CommunityTabs/CommunityTabs";
 import { NavigationContext } from "../Contexts";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Icon } from "@rneui/base";
+import Colors from "@/Constants/Colors";
+import ID from "@/Constants/ID";
+import { Overlay } from "@rneui/base";
 
+const account = new Account(client);
 const databases = new Databases(client);
 
 export const Community = (props) => {
   const windowHeight = Dimensions.get("window").height;
   const { navigation } = props;
 
+  const [notifs, setNotifs] = useState([]);
+  const [isOverlayVisible, setOverlayVisible] = useState(false);
+
+  useEffect(() => {
+    account
+      .get()
+      .then((response) => {
+        const user_id = response.$id;
+        console.log('dis you ' + user_id);
+        const promise = databases.listDocuments(
+          ID.mainDBID,
+          ID.notificationDataCenterCollectionID,
+          [
+            Query.equal("user_id", user_id),
+            Query.limit(10),
+          ],
+        );
+
+        promise.then(function (response) {
+          const documents = response.documents;
+          setNotifs(documents);
+        }).catch((error) => console.log(error));
+      });
+  }, [isOverlayVisible]);
+
   return (
     <NavigationContext.Provider value={navigation}>
       <SafeAreaView style={{ flexGrow: 1, backgroundColor: "white" }}>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-          <View>
+          <View style={{flexDirection: 'row'}}>
             <Text
               style={{
                 marginLeft: 20,
@@ -32,13 +65,47 @@ export const Community = (props) => {
                 marginTop: 5,
                 fontWeight: "700",
                 fontSize: 21,
+                flex: 5,
               }}
             >
               Community
             </Text>
+            <View style={{flex: 1, marginTop: 5, width: '100%'}}>
+              <TouchableOpacity onPress={() => setOverlayVisible(true)}>
+                <Icon name="notifications" color={ Colors.BUTTON_PURPLE }/>
+              </TouchableOpacity> 
+            </View>
           </View>
         </TouchableWithoutFeedback>
         <CommunityTabs />
+        <Overlay
+          isVisible={isOverlayVisible}
+          onBackdropPress={() => setOverlayVisible(false)}
+          statusBarTranslucent
+          overlayStyle={{width: 300, height: 500, backgroundColor: "white", borderRadius: 15}}
+        >
+          <View style={{alignItems: 'center'}}>
+            <Text style={{fontSize: 20, marginTop: 5}}>Notifications</Text>
+            <View style={{width: 300, height: 1, backgroundColor: '#d3d3d3', marginTop: 20}}/>
+            <View style={{height: 440}}>
+              <FlatList
+              data={notifs.reverse()}
+              ItemSeparatorComponent={() =>
+                <View style={{width: 300, height: 1, backgroundColor: '#d3d3d3'}}/>
+              }
+              renderItem={(item) => {
+                return (
+                  <View style={{marginTop: 10, marginBottom: 10}}>
+                    <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 2}}>{item.item.title}</Text>
+                    <Text>{item.item.description}</Text>
+                    <Text>{new Date(item.item.$createdAt).toLocaleString()}</Text>
+                  </View>
+                );
+              }}
+            />
+            </View>
+          </View>
+        </Overlay>
       </SafeAreaView>
     </NavigationContext.Provider>
   );
--- mobile/Screens/Library.tsx
+++ mobile/Screens/Library.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { View, Text, TouchableWithoutFeedback, Keyboard } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableWithoutFeedback, Keyboard, TouchableOpacity, FlatList } from "react-native";
 
 import CarouselTabs from "../Components/LibraryCarousel/LibraryCarouselTabs";
 
 import { NavigationContext } from "../Contexts";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BookSearchButton from "@/Components/BookSearchButton";
-import { Icon } from "@rneui/base";
-import Colors from "@/Constants/Colors";
+
 
 export const Library = (props) => {
   const { navigation } = props;
@@ -24,15 +23,11 @@ export const Library = (props) => {
                 marginTop: 5,
                 fontWeight: "700",
                 fontSize: 21,
-                flex: 5
               }}
             >
               Library
             </Text>
-            <View style={{flex: 1, marginTop: 5}}>
-              <Icon name="notifications" ></Icon>
-            </View>
-          </View>
+          </View>    
         </TouchableWithoutFeedback>
         <View style={{ paddingLeft: 10, paddingBottom: 10, paddingRight: 10 }}>
           <BookSearchButton
