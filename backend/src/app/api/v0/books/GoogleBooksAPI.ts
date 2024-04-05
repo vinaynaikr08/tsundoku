461e60de47567bd8c79767fca9b71c962b90b7e6
Eric Park <me@ericswpark.com>
2024-04-05 10:15:31 -0400

    backend: GoogleBooksAPI: remove unnecessary try-catch blocks

--- backend/src/app/api/v0/books/GoogleBooksAPI.ts
+++ backend/src/app/api/v0/books/GoogleBooksAPI.ts
@@ -7,37 +7,29 @@ function googleBooksAPIUnavailableError(res_json: any) {
 
 export namespace GoogleBooksAPI {
   export async function searchBooks(title: string) {
-    try {
-      const res = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(title)}`,
-      );
+    const res = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(title)}`,
+    );
 
-      const res_json = await res.json();
+    const res_json = await res.json();
 
-      if (typeof res_json === "undefined" || !("items" in res_json)) {
-        googleBooksAPIUnavailableError(res_json);
-      }
-
-      return res_json.items;
-    } catch (error) {
-      throw error;
+    if (typeof res_json === "undefined" || !("items" in res_json)) {
+      googleBooksAPIUnavailableError(res_json);
     }
+
+    return res_json.items;
   }
 
   export async function getBook(id: string) {
-    try {
-      const res = await fetch(
-        `https://www.googleapis.com/books/v1/volumes/${id}`,
-      );
+    const res = await fetch(
+      `https://www.googleapis.com/books/v1/volumes/${id}`,
+    );
 
-      const res_json = await res.json();
+    const res_json = await res.json();
 
-      if (typeof res_json === "undefined") {
-        googleBooksAPIUnavailableError(res_json);
-      }
-      return res_json;
-    } catch (error) {
-      throw error;
+    if (typeof res_json === "undefined") {
+      googleBooksAPIUnavailableError(res_json);
     }
+    return res_json;
   }
 }
