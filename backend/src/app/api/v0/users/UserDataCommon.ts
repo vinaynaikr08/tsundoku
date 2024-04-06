e6db5cee1aee425351bdd5e65e968a8aef4e09ed
Eric Park <me@ericswpark.com>
2024-04-06 16:12:05 -0400

    backend: api: users: fix bug with field fetching

--- backend/src/app/api/v0/users/UserDataCommon.ts
+++ backend/src/app/api/v0/users/UserDataCommon.ts
@@ -3,7 +3,7 @@ const sdk = require("node-appwrite");
 import { NextRequest, NextResponse } from "next/server";
 
 import Constants from "@/app/Constants";
-import { client, isAppwriteUserError } from "@/app/appwrite";
+import { client } from "@/app/appwrite";
 import { Query } from "node-appwrite";
 import {
   construct_development_api_response,
@@ -43,29 +43,25 @@ export async function Get(request: NextRequest, field_name: string) {
 
   let db_query;
   try {
-    db_query = await databases.getDocument(
+    db_query = await databases.listDocuments(
       Constants.MAIN_DB_ID,
       Constants.USERDATA_COL_ID,
       [Query.equal("user_id", user_id)],
     );
   } catch (error: any) {
-    if (
-      isAppwriteUserError(error) &&
-      //@ts-ignore: type not defined in AppwriteException
-      (error as AppwriteException).type === "document_not_found"
-    ) {
-      return construct_development_api_response({
-        message: `The user currently does not have a ${field_name} set.`,
-        response_name: field_name,
-      });
-    } else {
-      return handle_error(error);
-    }
+    return handle_error(error);
   }
 
-  return construct_development_api_response({
-    message: `Query succeeded.`,
-    response_data: db_query[field_name],
-    response_name: field_name,
-  });
+  if (db_query.documents.length === 1) {
+    return construct_development_api_response({
+      message: `Query succeeded.`,
+      response_data: db_query.documents[0][field_name],
+      response_name: field_name,
+    });
+  } else {
+    return construct_development_api_response({
+      message: `The user currently does not have a ${field_name} set.`,
+      response_name: field_name,
+    });
+  }
 }
