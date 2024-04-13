06dff53805c86bdc86a4d494a7bd0f86e5fad18c
Eric Park <me@ericswpark.com>
2024-04-13 14:43:35 -0400

    backend: api: add social URL endpoints

--- backend/docs/api/v0/User.md
+++ backend/docs/api/v0/User.md
@@ -104,6 +104,51 @@ Warning: you will not be able to set the about me bio if the `UserData` object d
 - `401` - if the auth token was not supplied or is invalid
 - `404` - the associated `UserData` entry was not found
 
+## Social URL
+
+### Query
+
+`host/api/v0/users/social_url`
+
+This endpoint allows you to query the social URL of the current user.
+
+All requests to this endpoint require the JWT token to be supplied in the `Authorization` header, in the `Bearer <token>` format.
+
+To query social URLs of other users, supply a user ID in the search parameter `user_id`.
+
+#### Method
+
+`GET`
+
+#### Search parameters (optional)
+
+- `user_id`
+
+#### Returns
+
+- `200` - request was successful
+- `400` - if neither the auth token nor the user ID was provided
+- `401` - if the auth token was not supplied or is invalid
+
+### Update
+
+Warning: you will not be able to set the social URL if the `UserData` object does not exist. This means the user account must have a `username` set in order to proceed.
+
+#### Method
+
+`PATCH`
+
+#### Body parameters (**required**)
+
+- `social_url`
+
+#### Returns
+
+- `200` - request was successful
+- `400` - if the required parameters are not supplied or are invalid
+- `401` - if the auth token was not supplied or is invalid
+- `404` - the associated `UserData` entry was not found
+
 ## Name query
 
 `host/api/v0/users/[user_id]/name`
---
+++ backend/src/app/api/v0/users/social_url/route.ts
@@ -0,0 +1,82 @@
+const sdk = require("node-appwrite");
+
+import Constants from "@/app/Constants";
+import { client } from "@/app/appwrite";
+import { NextRequest, NextResponse } from "next/server";
+import { ID, Query } from "node-appwrite";
+import {
+  construct_development_api_response,
+  handle_error,
+} from "../../dev_api_response";
+import { getOrFailAuthTokens } from "../../helpers";
+import { checkUserToken } from "../../userContext";
+import { Get } from "../UserDataCommon";
+
+const databases = new sdk.Databases(client);
+
+export async function GET(request: NextRequest) {
+  return await Get(request, "social_url");
+}
+
+export async function PATCH(request: NextRequest) {
+  const authToken = getOrFailAuthTokens();
+  if (authToken instanceof NextResponse) return authToken;
+
+  const user_id = await checkUserToken(authToken);
+  if (user_id instanceof NextResponse) return user_id;
+
+  let social_url;
+  try {
+    const data = await request.json();
+    social_url = data.social_url;
+  } catch (error) {
+    return construct_development_api_response({
+      message: "Bad request supplied.",
+      status_code: 400,
+    });
+  }
+
+  if (!social_url) {
+    return construct_development_api_response({
+      message: `You're trying to update the social URL, but didn't supply the new social URL as a parameter.`,
+      status_code: 400,
+    });
+  }
+
+  let userdata_id = null;
+  try {
+    const db_query = await databases.listDocuments(
+      Constants.MAIN_DB_ID,
+      Constants.USERDATA_COL_ID,
+      [Query.equal("user_id", user_id)],
+    );
+    if (db_query.documents.length === 1) {
+      userdata_id = db_query.documents[0].$id;
+    }
+  } catch (error: any) {
+    return handle_error(error);
+  }
+
+  if (!userdata_id) {
+    return construct_development_api_response({
+      message: `No UserData entry found. The user account must have a username to proceed.`,
+      status_code: 404,
+    });
+  } else {
+    try {
+      // Update existing userdata entry
+      await databases.updateDocument(
+        Constants.MAIN_DB_ID,
+        Constants.USERDATA_COL_ID,
+        userdata_id,
+        { social_url },
+      );
+    } catch (error: any) {
+      return handle_error(error);
+    }
+  }
+
+  return construct_development_api_response({
+    message: "The social URL was updated.",
+  });
+}
