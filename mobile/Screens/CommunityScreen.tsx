982dc4bb823b0188012de202173c628980b6b8b3
Eric Park <me@ericswpark.com>
2024-04-20 14:02:43 -0400

    mobile: CommunityScreen: remove unused variable and import

--- mobile/Screens/CommunityScreen.tsx
+++ mobile/Screens/CommunityScreen.tsx
@@ -1,14 +1,12 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
   TouchableWithoutFeedback,
   Keyboard,
-  Dimensions,
   StyleSheet,
   TouchableOpacity,
   FlatList,
-
 } from "react-native";
 import { Databases, Query, Account } from "appwrite";
 import { client } from "@/appwrite";
@@ -24,38 +22,34 @@ const account = new Account(client);
 const databases = new Databases(client);
 
 export const Community = (props) => {
-  const windowHeight = Dimensions.get("window").height;
   const { navigation } = props;
 
   const [notifs, setNotifs] = useState([]);
   const [isOverlayVisible, setOverlayVisible] = useState(false);
 
   useEffect(() => {
-    account
-      .get()
-      .then((response) => {
-        const user_id = response.$id;
-        const promise = databases.listDocuments(
-          ID.mainDBID,
-          ID.notificationDataCenterCollectionID,
-          [
-            Query.equal("user_id", user_id),
-            Query.limit(10),
-          ],
-        );
+    account.get().then((response) => {
+      const user_id = response.$id;
+      const promise = databases.listDocuments(
+        ID.mainDBID,
+        ID.notificationDataCenterCollectionID,
+        [Query.equal("user_id", user_id), Query.limit(10)],
+      );
 
-        promise.then(function (response) {
+      promise
+        .then(function (response) {
           const documents = response.documents;
           setNotifs(documents);
-        }).catch((error) => console.log(error));
-      });
+        })
+        .catch((error) => console.log(error));
+    });
   }, [isOverlayVisible]);
 
   return (
     <NavigationContext.Provider value={navigation}>
       <SafeAreaView style={{ flexGrow: 1, backgroundColor: "white" }}>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{ flexDirection: "row" }}>
             <Text
               style={{
                 marginLeft: 20,
@@ -68,10 +62,10 @@ export const Community = (props) => {
             >
               Community
             </Text>
-            <View style={{flex: 1, marginTop: 5, width: '100%'}}>
+            <View style={{ flex: 1, marginTop: 5, width: "100%" }}>
               <TouchableOpacity onPress={() => setOverlayVisible(true)}>
-                <Icon name="notifications" color={ Colors.BUTTON_PURPLE }/>
-              </TouchableOpacity> 
+                <Icon name="notifications" color={Colors.BUTTON_PURPLE} />
+              </TouchableOpacity>
             </View>
           </View>
         </TouchableWithoutFeedback>
@@ -80,28 +74,64 @@ export const Community = (props) => {
           isVisible={isOverlayVisible}
           onBackdropPress={() => setOverlayVisible(false)}
           statusBarTranslucent
-          overlayStyle={{width: 300, height: 600, backgroundColor: "white", borderRadius: 15}}
+          overlayStyle={{
+            width: 300,
+            height: 600,
+            backgroundColor: "white",
+            borderRadius: 15,
+          }}
         >
-          <View style={{alignItems: 'center'}}>
-            <Text style={{fontSize: 20, marginTop: 5}}>Notifications</Text>
-            <View style={{width: 300, height: 1, backgroundColor: '#d3d3d3', marginTop: 20}}/>
-            <View style={{height: 540}}>
-              <FlatList
-              data={notifs.reverse()}
-              showsVerticalScrollIndicator={false}
-              ItemSeparatorComponent={() =>
-                <View style={{width: '100%', height: 1, backgroundColor: '#d3d3d3', alignSelf: 'center'}}/>
-              }
-              renderItem={(item) => {
-                return (
-                  <View style={{marginTop: 10, marginBottom: 10, paddingRight: 10, paddingLeft: 10}}>
-                    <Text style={{fontWeight: 'bold', fontSize: 18, marginBottom: 2}}>{item.item.title}</Text>
-                    <Text>{item.item.description}</Text>
-                    <Text>{new Date(item.item.$createdAt).toLocaleString()}</Text>
-                  </View>
-                );
+          <View style={{ alignItems: "center" }}>
+            <Text style={{ fontSize: 20, marginTop: 5 }}>Notifications</Text>
+            <View
+              style={{
+                width: 300,
+                height: 1,
+                backgroundColor: "#d3d3d3",
+                marginTop: 20,
               }}
             />
+            <View style={{ height: 540 }}>
+              <FlatList
+                data={notifs.reverse()}
+                showsVerticalScrollIndicator={false}
+                ItemSeparatorComponent={() => (
+                  <View
+                    style={{
+                      width: "100%",
+                      height: 1,
+                      backgroundColor: "#d3d3d3",
+                      alignSelf: "center",
+                    }}
+                  />
+                )}
+                renderItem={(item) => {
+                  return (
+                    <View
+                      style={{
+                        marginTop: 10,
+                        marginBottom: 10,
+                        paddingRight: 10,
+                        paddingLeft: 10,
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontWeight: "bold",
+                          fontSize: 18,
+                          marginBottom: 2,
+                        }}
+                      >
+                        {item.item.title}
+                      </Text>
+                      <Text>{item.item.description}</Text>
+                      <Text>
+                        {new Date(item.item.$createdAt).toLocaleString()}
+                      </Text>
+                    </View>
+                  );
+                }}
+              />
             </View>
           </View>
         </Overlay>
