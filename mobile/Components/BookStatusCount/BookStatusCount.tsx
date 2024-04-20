66c44968e54a05007697314ae97816250dc7db91
Eric Park <me@ericswpark.com>
2024-04-20 15:02:50 -0400

    mobile: BookStatusCount: major refactor
    Closes #202

--- mobile/Components/BookStatusCount/BookStatusCount.tsx
+++ mobile/Components/BookStatusCount/BookStatusCount.tsx
@@ -1,84 +1,76 @@
-import ID from "@/Constants/ID";
-import { client } from "@/appwrite";
-import { Account, Databases, Query } from "appwrite";
+import Backend from "@/Backend";
+import Colors from "@/Constants/Colors";
+import { useFocusEffect } from "@react-navigation/native";
 import React from "react";
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { ActivityIndicator, ScrollView, StyleSheet, Text } from "react-native";
 import { Divider } from "react-native-paper";
+import useSWR from "swr";
+
+const backend = new Backend();
+
+async function fetchBookStatusCount() {
+  const bookStatusDocs = await backend.getBookStatusDocs({});
+
+  const readCount = bookStatusDocs.filter(
+    (doc) => doc.status === "READ",
+  ).length;
+  const currentlyReadingCount = bookStatusDocs.filter(
+    (doc) => doc.status === "CURRENTLY_READING",
+  ).length;
+  const wantToReadCount = bookStatusDocs.filter(
+    (doc) => doc.status === "WANT_TO_READ",
+  ).length;
+  const didNotFinishCount = bookStatusDocs.filter(
+    (doc) => doc.status === "DID_NOT_FINISH",
+  ).length;
+
+  return {
+    readCount,
+    currentlyReadingCount,
+    wantToReadCount,
+    didNotFinishCount,
+  };
+}
+
 function BookStatusCount() {
-  const [booksReadCount, setBooksReadCount] = React.useState(0);
-  const [booksCurrReadingCount, setBooksCurrReadingCount] = React.useState(0);
-  const [booksWantToReadCount, setBooksWantToReadCount] = React.useState(0);
-  const [booksDidNotFinishCount, setBooksDidNotFinishCount] = React.useState(0);
+  const { data, isLoading, error, mutate } = useSWR(
+    { func: fetchBookStatusCount, arg: null },
+    backend.swrFetcher,
+  );
 
-  React.useEffect(() => {
-    const account = new Account(client);
-    account
-      .get()
-      .then((response) => {
-        const user_id = response.$id;
-        const databases = new Databases(client);
-        const promise = databases.listDocuments(
-          ID.mainDBID,
-          ID.bookStatusCollectionID,
-          [Query.equal("user_id", user_id)],
-        );
+  useFocusEffect(
+    React.useCallback(() => {
+      mutate();
+    }, [mutate]),
+  );
 
-        promise.then(
-          function (response) {
-            const documents = response.documents;
-            let readCount = 0;
-            let currReadingCount = 0;
-            let wantToReadCount = 0;
-            let didNotFinishCount = 0;
-            documents.forEach((doc) => {
-              switch (doc.status) {
-                case "READ":
-                  readCount++;
-                  break;
-                case "CURRENTLY_READING":
-                  currReadingCount++;
-                  break;
-                case "WANT_TO_READ":
-                  wantToReadCount++;
-                  break;
-                case "DID_NOT_FINISH":
-                  didNotFinishCount++;
-                  break;
-                default:
-                  break;
-              }
-            });
-            setBooksReadCount(readCount);
-            setBooksCurrReadingCount(currReadingCount);
-            setBooksWantToReadCount(wantToReadCount);
-            setBooksDidNotFinishCount(didNotFinishCount);
-          },
-          function (error) {
-            console.log(error);
-          },
-        );
-      })
-      .catch((error) => {
-        console.error("Error fetching user ID:", error);
-      });
-  }, []);
+  if (isLoading) {
+    return (
+      <ActivityIndicator
+        size="large"
+        color={Colors.BUTTON_PURPLE}
+        style={{ marginTop: 50 }}
+      />
+    );
+  }
+
   return (
     <ScrollView
       testID="profile-tab-scroll-view"
-      style={{ flex: 1, marginLeft: -120 }}
+      style={styles.scrollViewStyle}
       bounces={false}
-      contentContainerStyle={styles.scrollViewStyle}
+      contentContainerStyle={styles.scrollViewContentStyle}
       showsVerticalScrollIndicator={false}
     >
-      <Text style={styles.text}>Books read: {booksReadCount}</Text>
+      <Text style={styles.text}>Books read: {data.readCount}</Text>
       <Text style={styles.text}>
-        Books currently reading: {booksCurrReadingCount}
+        Books currently reading: {data.currentlyReadingCount}
       </Text>
       <Text style={styles.text}>
-        Books want to read: {booksWantToReadCount}
+        Books want to read: {data.wantToReadCount}
       </Text>
       <Text style={styles.text}>
-        Books did not finish: {booksDidNotFinishCount}
+        Books did not finish: {data.didNotFinishCount}
       </Text>
       <Divider />
     </ScrollView>
@@ -88,7 +80,8 @@ function BookStatusCount() {
 export default BookStatusCount;
 
 const styles = StyleSheet.create({
-  scrollViewStyle: {
+  scrollViewStyle: { flex: 1, marginLeft: -120 },
+  scrollViewContentStyle: {
     paddingHorizontal: 5,
     backgroundColor: "white",
   },
