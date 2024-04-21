27afd40c508cc5f2601e8a7919acd58d70d35b1b
Eric Park <me@ericswpark.com>
2024-04-21 11:12:10 -0400

    mobile: BookInfoWrapper: fix React import

--- mobile/Components/BookInfoWrapper/BookInfoWrapper.tsx
+++ mobile/Components/BookInfoWrapper/BookInfoWrapper.tsx
@@ -1,5 +1,5 @@
 import { BookInfoWrapperContext } from "@/Contexts";
-import * as React from "react";
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { BookInfoModal } from "../BookInfoModal";
 import FullReview from "../FullReview/FullReview";
