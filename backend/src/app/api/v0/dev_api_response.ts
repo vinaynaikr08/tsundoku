e9a72d63174d17d0f62194b3b7c17f059069377f
Eric Park <me@ericswpark.com>
2024-04-25 11:00:47 -0400

    Revert "backend: dev_api_response: remove warning message"
    This reverts commit bd573e6b25dfadbaff901ac9d242598db640ce03.

--- backend/src/app/api/v0/dev_api_response.ts
+++ backend/src/app/api/v0/dev_api_response.ts
@@ -32,6 +32,8 @@ export function construct_development_api_response({
     return NextResponse.json(
       {
         message,
+        warning:
+          "You are calling a development API! The schema may change without warning.",
         [response_name]: response_data,
       },
       { status: status_code },
@@ -40,6 +42,8 @@ export function construct_development_api_response({
   return NextResponse.json(
     {
       message,
+      warning:
+        "You are calling a development API! The schema may change without warning.",
     },
     { status: status_code },
   );
